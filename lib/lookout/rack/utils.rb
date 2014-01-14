747f02aeb586962cb0606fed5a4126bb53e2c715
Ian Smith <ian.smith@mylookout.com>
2014-01-13 16:52:17 -0800

    We don't need lookout-ruby here, I think, just the YAML monkeypatch.

--- lib/lookout/rack/utils.rb
+++ lib/lookout/rack/utils.rb
@@ -7,6 +7,16 @@ module Lookout
   end
 end
 
+if RUBY_PLATFORM == 'java'
+  # This is required because YAML is messed up and incompatible with our version
+  # of Configatron under JRuby
+  module Psych
+    module Yecht
+      MergeKey = nil
+    end
+  end
+end
+
 require 'lookout/rack/utils/graphite'
 require 'lookout/rack/utils/i18n'
 require 'lookout/rack/utils/log'
