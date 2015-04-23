2f8aa86fd3f14fd0d486653bad034c4afbf7a823
Sivasankar Ramasubramanian <sivasankar.ramasubramanian@lookout.com>
2015-04-23 13:42:03 -0700

    Add unzipping functionality to Request module

--- lib/lookout/rack/utils/request.rb
+++ lib/lookout/rack/utils/request.rb
@@ -1,6 +1,10 @@
+require 'zlib'
+
 module Lookout::Rack::Utils
   module Request
     ILLEGAL_CHARS_REGEX = /[<>]/
+    HTTP_CONTENT_ENCODING_KEY = "HTTP_CONTENT_ENCODING"
+    CONTENT_ENCODING_GZIPPED = "gzip"
 
     # Return the raw, unprocessed request body
     #
@@ -11,11 +15,33 @@ module Lookout::Rack::Utils
       return request.body.read
     end
 
-    # Process and parse the request body as JSON
+    # Return the gunzipped but otherwise raw, unprocessed request body
     #
     # Will halt and create a a 400 status code if there is something wrong with
     # the body
     #
+    # @return  [String]
+    def gunzipped_body
+      body = raw_body
+      if request.env[HTTP_CONTENT_ENCODING_KEY] != CONTENT_ENCODING_GZIPPED
+        return body
+      else
+        begin
+          return gunzip(body)
+        rescue Zlib::Error
+          if defined?(Lookout::Rack::Utils::Log)
+            Lookout::Rack::Utils::Log.instance.warn "Unzipping error when decompressing request body (#{body})"
+          end
+          halt 400, "{}"
+        end
+      end
+    end
+
+    # Process and parse the request body as JSON
+    #
+    # Will halt and create a a 400 status code if there is something wrong with
+    # the body
+    #s
     def body_as_json
       body = raw_body
 
@@ -31,5 +57,24 @@ module Lookout::Rack::Utils
         halt 400, "{}"
       end
     end
+
+    private
+
+    # Decompresses data
+    #
+    # @raise [Zlib::Error] if malformed data is provided
+    # @return [String]
+    def gunzip(data)
+      # We set the window bits to MAX_WBITS + 32 to enable zlib and gzip decoding
+      # with automatic header detection.
+      zstream = Zlib::Inflate.new(Zlib::MAX_WBITS+32)
+      begin
+        decompressed = zstream.inflate(data)
+      ensure
+        zstream.close
+      end
+      decompressed
+    end
+
   end
 end
